48557779907cd82b6aadb35dd2e20d01b797b552
KaminaSan <>
2016-03-18 16:10:59 +0900

    Created logout controller

---
+++ src/java/com/mhs/controller/LogoutController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mhs.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author KaminaSan <www.kaminasan.com>
+ */
+
+@WebServlet(name="LogoutController", urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
+   
+   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        ServletContext sc = request.getServletContext();
+        HttpSession sesh = request.getSession();
+        PrintWriter writer = response.getWriter();
+        if(!(sesh == null)){
+            sesh.invalidate();
+            response.setStatus(200);
+            writer.print("Logged Out!");
+            
+        }
+        else{
+            response.setStatus(401);
+            writer.print("Error, not logged in, so you cannot log out.");
+            
+        }
+     
+ 
+    } 
+
+ 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
